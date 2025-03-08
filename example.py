ff4c8fde284e31c914edcbc8ce725873ac96ee61
Sean S <39308550+pitterpatter22@users.noreply.github.com>
2025-03-07 19:52:13 -0500

    Update example.py

--- example.py
+++ example.py
@@ -9,7 +9,12 @@ def custom():
     input("Press enter to kill this!!!")
     app.exit_app()
 
+# With optional attributes
 app = easymenu(name="My Custom App", author="Joe Schmo", url="https://github.com", url_label="My Site", quit_item=True, debug=False, make_screen=True)
+
+# Most Basic
+#app = easymenu()
+
 app.add_menu_option(item_name="Option 1", action=example1)
 app.add_menu_option(item_name="Option 2", action=example2)
 app.add_menu_option(item_name="Custom Option", action=custom, item_key="c", order_weight=1, color='\033[92m')
