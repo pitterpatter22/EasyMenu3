9a982afa9aa242f88d94c926f5a51818e281324d
Sean S <39308550+pitterpatter22@users.noreply.github.com>
2025-03-07 19:31:22 -0500

    Add files via upload

---
+++ example.py
@@ -0,0 +1,18 @@
+from EasyMenu3 import easymenu
+def example1():
+    app.print_success("example1 ran")
+def example2():
+    app.print_error("example2 ran")
+    
+def custom():
+    app.print_info("Custom Option")
+    input("Press enter to kill this!!!")
+    app.exit_app()
+
+app = easymenu(name="My Custom App", author="Joe Schmo", url="https://github.com", url_label="My Site", quit_item=True, debug=False, make_screen=True)
+app.add_menu_option(item_name="Option 1", action=example1)
+app.add_menu_option(item_name="Option 2", action=example2)
+app.add_menu_option(item_name="Custom Option", action=custom, item_key="c", order_weight=1, color='\033[92m')
+
+#app.print_menu()
+app.start()
